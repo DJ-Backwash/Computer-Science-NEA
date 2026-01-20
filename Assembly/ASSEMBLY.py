3464a6bc3ff79cd2ed9e62dfdb2c0c6b0f3010f8
DJ-Backwash <joshiwashi08@gmail.com>
2026-01-20 11:48:10 +0000

    Big update to functionality

--- Assembly/ASSEMBLY.py
+++ Assembly/ASSEMBLY.py
@@ -1,29 +1,41 @@
-# Although the code here could be shortened by many lines using functions and methods
-# i did not do this as i am lazy and tired. 
-#
-# on program exit any memory locations will be saved and overwritten in a file called "memory.txt"
+# Notes for me so i don't forget
+
+# On program exit any memory locations will be saved and overwritten in a file called "memory.txt"
 # and are able to be loaded from in a new program
 #
 # The syntax for the assembly is the same as the one used by AQA however commas are not
 # neccesary, they will be ignored by the program. 
-# 2 new instructions were added to assist with debugging and use of your programs:
+# 2 new instructions were added to assist with debugging and use of programs:
 #
 #   OUT:
 #       this will add whatever you give it to the output log, if multiple things are 
-#       added they will placed in a list. * redundant *
+#       added they will placed in a list.
 #
 #   PRT:
 #       this will immediately output whatever is fed into it when the line is read and 
 #       was implemented to assist with debugging.
-import sys
 
 # code
-def run(filename = "mycode.txt"): # name of the file containing your assembly code, if no 
+registers = None
+
+def refresh_reg():
+    global registers
+    # When the program closes reset Registers and Memory
+    registers = {}
+    reg_names = ["R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12"]
+    for reg in reg_names:
+        registers[reg] = 0
+    with open("registers.txt", "w") as f:
+        f.write(str(registers))
+    
+
+def run_code(FrontEnd, filename = "mycode.txt", mode = 0): # name of the file containing your assembly code, if no 
 #                                 name is passed then it will be automatically loaded 
-#                                 from "code.txt" to use your own file name edit the 
+#                                 from "mycode.txt" to use your own file name edit the 
 #                                 "print(run())" at the end of this file.
 
     #grab file and place each line in its own spot in a list
+    global contents, print_log, read_head, cmp, registers, memory, labels, looping
     contents = []
     try:
         with open(filename, 'r') as f:
@@ -41,10 +53,7 @@ def run(filename = "mycode.txt"): # name of the file containing your assembly co
     cmp = []
 
     #create "registers"
-    registers = {}
-    reg_names = ["R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10", "R11", "R12"]
-    for reg in reg_names:
-        registers[reg] = 0
+    refresh_reg()
 
     #initialise memory, feel free to add values to memory.txt
     try:
@@ -60,179 +69,190 @@ def run(filename = "mycode.txt"): # name of the file containing your assembly co
             labels[contents[i][:-1]] = i
 
     #main sequence
-    while True:
-        del_cmp = True # this line is explained lower
+    if mode == 0:
+        looping = None
+        while looping != "HALT":
+            looping = execute(FrontEnd)
+        FrontEnd.console_out(f"HALT reached", "Success")
 
-        #chech for end of file in if 'HALT' is forgotten
-        if read_head == len(contents):
-            break
+def execute(FrontEnd):
+    global contents, print_log, read_head, cmp, registers, memory, labels, looping
+
+    del_cmp = True # this line is explained lower
+    
+    #current line
+    line = contents[read_head]
+
+    #split line into list of parts
+    parts = line.split(" ")
+
+    match parts[0]:
+        case "OUT": # Rn
+            n1 = int(registers[parts[1]])
+            print_log.append(n1)
+
+        case "MOV": # Rd <operand2>
+            if parts[2][0] == "#": # instant addressing
+                n1 = int(parts[2][1:])
+
+            else: # direct addressing
+                n1 = registers[parts[2]]
+
+            registers[parts[1]] = int(n1)
         
-        #current line
-        line = contents[read_head]
+        case "ADD": # Rd Rn <operand2>
+            n1 = registers[parts[2]]
 
-        #split line into list of parts
-        parts = line.split(" ")
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][1:])
 
-        match parts[0]:
-            case "OUT": # Rn
-                n1 = int(registers[parts[1]])
-                print_log.append(n1)
-
-            case "MOV": # Rd <operand2>
-                if parts[2][0] == "#": # instant addressing
-                    n1 = int(parts[2][1:])
-
-                else: # direct addressing
-                    n1 = registers[parts[2]]
-
-                registers[parts[1]] = int(n1)
+            else: # direct addressing
+                n2 = registers[parts[3]]
             
-            case "ADD": # Rd Rn <operand2>
-                n1 = registers[parts[2]]
+            registers[parts[1]] = n1 + n2
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][1:])
+        case "SUB": # Rd Rn <operand2>
+            n1 = registers[parts[2]]
 
-                else: # direct addressing
-                    n2 = registers[parts[3]]
-                
-                registers[parts[1]] = n1 + n2
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][1:])
 
-            case "SUB": # Rd Rn <operand2>
-                n1 = registers[parts[2]]
+            else: # direct addressing
+                n2 = registers[parts[3]]
+            
+            registers[parts[1]] = n1 - n2
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][1:])
+        case "LSL": # Rd Rn <operand2>
+            n1 = registers[parts[2]]
 
-                else: # direct addressing
-                    n2 = registers[parts[3]]
-                
-                registers[parts[1]] = n1 - n2
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][1:])
 
-            case "LSL": # Rd Rn <operand2>
-                n1 = registers[parts[2]]
+            else: # direct addressing
+                n2 = registers[parts[3]]
+            
+            registers[parts[1]] = n1 << n2
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][1:])
+        case "LSR": # Rd Rn <operand2>
+            n1 = registers[parts[2]]
 
-                else: # direct addressing
-                    n2 = registers[parts[3]]
-                
-                registers[parts[1]] = n1 << n2
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][1:])
 
-            case "LSR": # Rd Rn <operand2>
-                n1 = registers[parts[2]]
+            else: # direct addressing
+                n2 = registers[parts[3]]
+            
+            registers[parts[1]] = n1 >> n2
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][1:])
+        case "AND": # Rd Rn <operand2>
+            n1 = bin(registers[parts[2]])[-1]
 
-                else: # direct addressing
-                    n2 = registers[parts[3]]
-                
-                registers[parts[1]] = n1 >> n2
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][-1])
 
-            case "AND": # Rd Rn <operand2>
+            else: # direct addressing
+                n2 = bin(registers[parts[3]])[-1]
+            
+            registers[parts[1]] = n1 and n2
+
+        case "ORR": # Rd Rn <operand2>
+            n1 = bin(registers[parts[2]])[-1]
+
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][-1])
+
+            else: # direct addressing
+                n2 = bin(registers[parts[3]])[-1]
+            
+            registers[parts[1]] = n1 or n2
+
+        case "EOR": # Rd Rn <operand2>
+            n1 = bin(registers[parts[2]])[-1]
+
+            if parts[3][0] == "#": # instant addressing
+                n2 = int(parts[3][-1])
+
+            else: # direct addressing
+                n2 = bin(registers[parts[3]])[-1]
+            
+            registers[parts[1]] = n1 != n2
+
+        case "MVN": # Rd <operand2>
+            if parts[2][0] == "#": # instant addressing
+                n1 = int(parts[2][-1])
+
+            else: # direct addressing
                 n1 = bin(registers[parts[2]])[-1]
+            
+            registers[parts[1]] = not n1
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][-1])
+        case "PRT": # <operand>; intended for bugfixing, final outputs should use "OUT"
+            if parts[1][0] == "#": # instant addressing
+                print(int(parts[1][1:]))
 
-                else: # direct addressing
-                    n2 = bin(registers[parts[3]])[-1]
-                
-                registers[parts[1]] = n1 and n2
+            else: # direct addressing
+                print(registers[parts[1]])
 
-            case "ORR": # Rd Rn <operand2>
-                n1 = bin(registers[parts[2]])[-1]
+        #branches
+        case "B": # <label>
+            read_head = labels[parts[1]]
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][-1])
+        case "CMP": # Rn <operand2>
+            del_cmp = False
+            cmp.append(registers[parts[1]])
 
-                else: # direct addressing
-                    n2 = bin(registers[parts[3]])[-1]
-                
-                registers[parts[1]] = n1 or n2
+            if parts[2][0] == "#": # instant addressing
+                cmp.append(int(parts[2][1:]))
 
-            case "EOR": # Rd Rn <operand2>
-                n1 = bin(registers[parts[2]])[-1]
+            else: # direct addressing
+                cmp.append(registers[parts[2]])
 
-                if parts[3][0] == "#": # instant addressing
-                    n2 = int(parts[3][-1])
-
-                else: # direct addressing
-                    n2 = bin(registers[parts[3]])[-1]
-                
-                registers[parts[1]] = n1 != n2
-
-            case "MVN": # Rd <operand2>
-                if parts[2][0] == "#": # instant addressing
-                    n1 = int(parts[2][-1])
-
-                else: # direct addressing
-                    n1 = bin(registers[parts[2]])[-1]
-                
-                registers[parts[1]] = not n1
-
-            case "PRT": # <operand>; intended for bugfixing, final outputs should use "OUT"
-                if parts[1][0] == "#": # instant addressing
-                    print(int(parts[1][1:]))
-
-                else: # direct addressing
-                    print(registers[parts[1]])
-
-            #branches
-            case "B": # <label>
+        case "BEQ": # <label>
+            del_cmp = False
+            if cmp[0] == cmp[1]:
                 read_head = labels[parts[1]]
 
-            case "CMP": # Rn <operand2>
-                del_cmp = False
-                cmp.append(registers[parts[1]])
+        case "BNE": # <label>
+            del_cmp = False
+            if cmp[0] != cmp[1]:
+                read_head = labels[parts[1]]
 
-                if parts[2][0] == "#": # instant addressing
-                    cmp.append(int(parts[2][1:]))
+        case "BLT": # <label>
+            del_cmp = False
+            if cmp[0] < cmp[1]:
+                read_head = labels[parts[1]]
 
-                else: # direct addressing
-                    cmp.append(registers[parts[2]])
+        case "BGT": # <label>
+            del_cmp = False
+            if cmp[0] < cmp[1]:
+                read_head = labels[parts[1]]
 
-            case "BEQ": # <label>
-                del_cmp = False
-                if cmp[0] == cmp[1]:
-                    read_head = labels[parts[1]]
+        case "STR": # Rd <memory ref>
+            memory[parts[2]] = registers[parts[1]]
 
-            case "BNE": # <label>
-                del_cmp = False
-                if cmp[0] != cmp[1]:
-                    read_head = labels[parts[1]]
+        case "LDR": # Rd <memory ref>
+            registers[parts[1]] = memory[parts[2]]
+            
+        case "HALT": # Stop!!!
+            return "HALT"
 
-            case "BLT": # <label>
-                del_cmp = False
-                if cmp[0] < cmp[1]:
-                    read_head = labels[parts[1]]
+        case _: #its a label, don't do anything
+            pass
 
-            case "BGT": # <label>
-                del_cmp = False
-                if cmp[0] < cmp[1]:
-                    read_head = labels[parts[1]]
+    if del_cmp == True: # effectively, this stores the comparison variables until a line that does not contain a branch is reached
+        cmp = []
+    
+    read_head += 1
+    
+    # Update register file
+    with open("registers.txt", "w") as f:
+        f.write(str(registers))
 
-            case "STR": # Rd <memory ref>
-                memory[parts[2]] = registers[parts[1]]
-
-            case "LDR": # Rd <memory ref>
-                registers[parts[1]] = memory[parts[2]]
-                
-            case "HALT": # Stop!!!
-                break
-
-            case _: #its a label, don't do anything
-                pass
-
-        if del_cmp == True: # effectively, this stores the comparison variables until a line that does not contain a branch is reached
-            cmp = []
-
-        read_head += 1
 
     with open("memory.txt", 'w') as f:
         f.write(str(memory))
-    return print_log
+    FrontEnd.draw_registers()
 
-print(run(sys.argv[1]))
+    if print_log != []:
+        FrontEnd.console_out(print_log, "Out")
+    return read_head
